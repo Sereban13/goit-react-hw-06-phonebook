70e139ab844ba6f1967e607ae9133552f498f460
Sehii Perevertniuk <s.perevertnyuk@gmail.com>
2023-09-13 20:58:39 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
+import { useEffect, useState } from 'react';
+import { Section, Title } from './App.Styled';
+import { GlobalStyle } from './GlobalStyle';
+import { ContactForm } from './ContactForm/ContactForm';
+import { SearchBar } from './Searchbar/Searchbar';
+import { ContactList } from './ContactList/ContactList';
+
+const baseContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+const localStorageKey = 'Home-Contacts';
+
+const contactsFromLocalStorage = () => {
+  const savedContacts = localStorage.getItem(localStorageKey);
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
+  }
+  return baseContacts;
+};
+
 export const App = () => {
+  const [contacts, setContacts] = useState(contactsFromLocalStorage);
+  const [filter, setFilter] = useState('');
+
+  const changeNameFilter = newName => {
+    setFilter(newName);
+  };
+
+  const addContact = newContact => {
+    const isExistName = contacts.find(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+    if (isExistName) {
+      alert(`Contact "${newContact.name}" is already exist`);
+      return;
+    }
+    setContacts(contacts => [...contacts, newContact]);
+  };
+
+  const deleteContact = сontactId => {
+    setContacts(contacts =>
+      contacts.filter(contact => contact.id !== сontactId)
+    );
+  };
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Section>
+      <Title>Phonebook</Title>
+
+      <ContactForm onAdd={addContact} contacts={contacts} />
+
+      <Title>Contacts</Title>
+
+      <SearchBar filterName={filter} onChangeName={changeNameFilter} />
+
+      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
+
+      <GlobalStyle />
+    </Section>
   );
 };
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,6 +1,8 @@
-@import-normalize; /* bring in normalize.css styles */
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
 
-body {
+export const GlobalStyle = createGlobalStyle`
+  body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
@@ -13,3 +15,25 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+ul,
+ol {
+    /* list-style: none; */
+    margin: 0;
+    padding: 0;
+}
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+`;
